f3a983b6edcf132cb3c6c6b166ed77440254a5d5
exospherian <hialuron92@gmail.com>
2024-02-13 17:10:43 +0400

    npm init & minor fix

--- promises/general_promise.js
+++ promises/general_promise.js
@@ -1,8 +1,8 @@
-let i = new Promise();
+//let i = new Promise();
 
 let k = new Promise(()=> console.log(1));
 
-i.then(() => console.log(1));
+//i.then(() => console.log(1));
 
 k.then(() => console.log(2));
 
